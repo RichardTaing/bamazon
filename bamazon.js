b9df6112f8e70104cd68a96716a05924b251fcea
Richard Taing <Richard.Taing@pexa.com.au>
2019-09-06 14:19:16 +1000

    Completed bamazon.js and bamazon.sql

--- bamazon.js
+++ bamazon.js
@@ -16,3 +16,105 @@ var connection = mysql.createConnection({
   password: "MonashBootCamp2019",
   database: "bamazon"
 });
+
+connection.connect(function(err) {
+  if (err) throw err;
+});
+connection.query("SELECT * FROM products", function(err, res) {
+  //empty array for the attributes for each product
+  values = [];
+  console.log(
+    "Welcome to BBQ's online, here you will find products for your BBQing needs: \n"
+  );
+  //Loops through all the products on the database
+  for (i = 0; i < res.length; i++) {
+    //for each i it will push the product into an array and then pushes that array into the empty values array
+    array = [];
+    array.push(res[i].id, res[i].product_name, res[i].price);
+    values.push(array);
+  }
+  console.log("\n------------------------------------------");
+  //this is using the console.table cdn the values are set in the brackets that the values arrays goes in
+  console.table(["ID", "Name", "Price"], values);
+  console.log("\n------------------------------------------");
+  //starts the app
+  initialize();
+});
+
+initialize = () => {
+  //asks the user what they want to buy
+  inquirer
+    .prompt({
+      name: "productId",
+      type: "input",
+      message: "what is the id of the product you want to buy"
+    })
+    //callback to ask them how many they want to buy
+    .then(function(data) {
+      query = "SELECT * FROM products where id=" + data.productId;
+      connection.query(query, function(err, res) {
+        inquirer
+          .prompt({
+            name: "skies",
+            type: "input",
+            message:
+              "How many " + res[0].product_name + " would you like to purchase?"
+          })
+          .then(function(data) {
+            //variable to show the new quantity in the database
+            var newQuant = res[0].stock_quantity - data.skies;
+            //varaible to show how much the units * price costs
+            var totalCost = res[0].price * data.skies;
+            //variable to show the total sales of that product in the database
+            var totalCost1 = res[0].product_sales + totalCost;
+            if (data.skies > res[0].stock_quantity) {
+              console.log(
+                "That's too much, I don't have enough of that product"
+              );
+            } else {
+              //changes the quantity and item sales in the database
+              connection.query("UPDATE products SET ? WHERE ?", [
+                {
+                  stock_quantity: newQuant,
+                  product_sales: totalCost1
+                },
+                {
+                  id: res[0].id
+                }
+              ]);
+
+              console.log(
+                "Your order of " +
+                  data.skies +
+                  " " +
+                  res[0].product_name +
+                  " has been processed it will cost " +
+                  "$" +
+                  totalCost +
+                  "\n"
+              );
+              anyMore();
+            }
+          });
+      });
+    });
+};
+//after it prompts the user if they want to still shop
+anyMore = () => {
+  inquirer
+    .prompt({
+      name: "more",
+      type: "confirm",
+      message: "Do you want to buy anthing else?"
+    })
+    .then(function(data) {
+      if (data.more) {
+        initialize();
+      } else {
+        console.log(
+          "\nGoodbye Sir/Madam\n\n Thank you for shopping at BBQ's online\n"
+        );
+        process.exit();
+      }
+    });
+};
---
+++ bamazon.sql
@@ -0,0 +1,57 @@
+
+-- creates the database and the table products 
+create database bamazon;
+create table products
+(
+    --the characteristics of all the fields in the products table
+    id int not null
+    auto_increment,
+product_name varchar
+    (50),
+department_name varchar
+    (50),
+price int,
+stock_quantity int,
+primary key
+    (id)
+
+);
+    --populates the initial retail
+    insert into products
+        (product_name, department_name, price, stock_quantity)
+    values
+        ('Texas Offset Smoker', 'Barbeques', 999, 20 ),
+        ('Premium Beefmaster 6 Burner', 'Barbeques', 1299, 5),
+        ('Smokin Cow Beef Brisket Rub 300G', 'BBQ Pantry', 14.95, 24),
+        ('Whisky Maple BBQSauce', 'BBQ Pantry', 11.95, 12),
+        ('Rub Your Butt BBQ Rubs - 3 Pack', 'BBQ Pantry', 32.95, 18),
+        ('Monster BBQ Pack', 'Bulk Meat Packs', 299, 20),
+        ('Ziggy Ziegler & Brown Twin Grill', 'Barbeques', 718, 4),
+        ('Great Southern Lamb Legs Boned & Rolled 1.8kg - 2kg', 'Weekly Specials', 32, 7),
+        ('Stubbs Ass Kickin Mini Chilli Set', 'BBQ Pantry', 7.95, 25),
+        ('Arrosto Gas Smoker', 'Barbeques', 329, 2);
+    --adds the produc_sales field for part 3
+    ALTER TABLE products ADD COLUMN product_sales int;
+    --populates the department table with name and dummy value for overhead
+    INSERT INTO departments
+        (department_name, overhead)
+    values
+        ('Barbeques', 50),
+        ('BBQ Pantry', 60),
+        ('Bulk Meat Packs', 20),
+        ('Weekly Specials', 15),
+        ('Heating', 30);
+    --creates the departments table for part 3
+    CREATE TABLE departments
+    (
+        department_id int not null
+        auto_increment,
+  department_name varchar
+        (50),
+  overhead int,
+  primary key
+        (department_id));
+        select *
+        from departments;
+        select *
+        from products;
